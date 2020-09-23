813ab8ee1391cd7b6191f67eed4b306ee6dc5c69
guillenjoshua <guillenjoshua@gmail.com>
2020-09-23 11:30:36 -0500

    cleaned up api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -14,49 +14,47 @@ const uniqid = require('uniqid')
 module.exports = function(app) {
 
     
-    app.get("/api/notes", (req, res) => {
-        res.json(notesApp);
-      });
-
-      //POST - add to notes, function called from ln 46
-      app.post("/api/notes", (req, res) => {
-
-                const addNote = req.body
-
-                    addNote.id = uniqid()
-                    notesApp.push(addNote)
-                    addToNotes();
-                    res.send('this worked!')
-                    return console.log("Added new note: " +addNote.title);
-                });
-
-                //GET id from notes
-                app.get(".api/notes/:id", (req, res) => {
-                    res.json(notesApp[req.params.id]);
-                })
-
-                //Delete notes with ID
-                app.delete("/api/notes/:id", (req, res) => {
-                    notesApp = notesApp.filter(
-                        note => {
-                            return note.id != req.params.id
-                        }
-                    )
-                     addToNotes();
-                     res.send('this worked!')
+        app.get("/api/notes", (req, res) => {
+            res.json(notesApp);
                     });
 
-                //Function to add to notes, which is called in POST
-                    function addToNotes() {
-                      const dataJSONAdd = JSON.stringify(notesApp)
-                      fs.writeFileSync('./db/db.json', dataJSONAdd, err => {
-                        if (err) throw err;
-                        return true;
-                      })
-                      
-                  }
-                        
+      //POST - add to notes, function called from ln 46
+        app.post("/api/notes", (req, res) => {
 
+            const addNote = req.body
+
+                addNote.id = uniqid()
+                notesApp.push(addNote)
+                addToNotes();
+                res.send();
+                    });
+
+                //GET id from notes
+        app.get(".api/notes/:id", (req, res) => {
+                res.json(notesApp[req.params.id]);
+                    })
+
+                //Delete notes with ID
+        app.delete("/api/notes/:id", (req, res) => {
+                notesApp = notesApp.filter(
+                    note => {
+                        return note.id != req.params.id
+                    }
+                )
+                    addToNotes();
+                    res.send();
+            });
+
+                //Function to add to notes, which is called in POST
+            function addToNotes() {
+                    const dataJSONAdd = JSON.stringify(notesApp)
+                    fs.writeFileSync('./db/db.json', dataJSONAdd, err => {
+                    if (err) throw err;
+                    return true;
+                    })
+                      
+                }
+              
 }
 
     
